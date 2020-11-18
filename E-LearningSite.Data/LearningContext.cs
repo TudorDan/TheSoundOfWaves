ccd57817feaf126750ae15e0e7da52e40ee51724
TudorDan <tudor.ist@gmail.com>
2020-11-18 15:05:32 +0200

    Add initial migration

--- E-LearningSite.Data/LearningContext.cs
+++ E-LearningSite.Data/LearningContext.cs
@@ -1,9 +1,37 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using E_LearningSite.Domain;
+using Microsoft.EntityFrameworkCore;
 using System;
 
 namespace E_LearningSite.Data
 {
     public class LearningContext : DbContext
     {
+        public DbSet<School> Schools { get; set; }
+        public DbSet<Principal> Principals { get; set; }
+        public DbSet<Mentor> Mentors { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Document> Documents { get; set; }
+        public DbSet<Catalogue> Catalogues { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = LearningApp");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<MentorCatalogue>().HasKey(m => new { m.MentorId, m.CatalogueId });
+            modelBuilder.Entity<CourseCatalogue>().HasKey(c => new { c.CourseId, c.CatalogueId });
+
+            modelBuilder.Entity<School>().HasMany(s => s.StudentsList)
+                .WithOne(s => s.School).OnDelete(DeleteBehavior.Restrict);
+            modelBuilder.Entity<School>().HasMany(s => s.CoursesList)
+                .WithOne(c => c.School).OnDelete(DeleteBehavior.Restrict);
+            modelBuilder.Entity<School>().HasMany(s => s.MentorsList)
+                .WithOne(m => m.School).OnDelete(DeleteBehavior.Restrict);
+        }
     }
 }
